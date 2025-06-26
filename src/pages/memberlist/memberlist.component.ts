2dcbc64f6b54b95e67f1b92d05a9d58cdc58906e
abhisekghosh018 <abhisekghosh018@gmail.com>
2025-06-26 22:11:18 +0530

    mamber list componenet adds

---
+++ src/pages/memberlist/memberlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'taskly-memberlist',
+  imports: [],
+  templateUrl: './memberlist.component.html',
+  styleUrl: './memberlist.component.css'
+})
+export class MemberlistComponent {
+
+}
